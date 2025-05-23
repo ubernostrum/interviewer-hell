d481b6a4be7f8ca09ab1cf1698bfb115a54123af
James Bennett <james@b-list.org>
2025-05-22 18:19:20 -0700

    Apply refactorings and best practices in C# square detector.

--- squares/SquareDetector.cs
+++ squares/SquareDetector.cs
@@ -1,41 +1,40 @@
-﻿using System.Collections.Generic;
+﻿namespace SquareDetector;
 
-static class SquareDetector
+internal static class SquareDetector
 {
-    static IEnumerable<int> Count()
+    private static IEnumerable<int> Count()
     {
-        int current = 0;
+        var current = 0;
         while (true)
             yield return current++;
     }
 
-    static IEnumerable<long> Accumulate(this IEnumerable<int> source)
+    private static IEnumerable<long> Accumulate(this IEnumerable<int> source)
     {
         long sum = 0;
-        foreach(int i in source)
+        foreach (var i in source)
         {
             sum += i;
             yield return sum;
         }
     }
 
-    /// <summary>Determines whether the given integer is a perfect square.</summary>
-    static bool IsSquare(int n) =>
-        n < 0 ? false :
-        n == 0 ? true :
-        Count().Where(i => (i & 1) == 1).Accumulate().TakeWhile(s => s <= n).Contains(n);
+    /// <summary>
+    /// Determine whether the given value is a perfect square.
+    /// </summary>
+    /// <param name="n">The value to check.</param>
+    /// <returns>Whether the given value is a perfect square.</returns>
+    private static bool IsSquare(int n) =>
+        n >= 0 && (n == 0 || Count().Where(i => (i & 1) == 1).Accumulate().TakeWhile(s => s <= n).Contains(n));
 
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        int input;
-        if (args.Length < 1 || !int.TryParse(args[0], out input))
+        if (args.Length < 1 || !int.TryParse(args[0], out var input))
         {
             Console.WriteLine("Please provide a numeric argument.");
             return;
         }
-        if (IsSquare(input))
-            Console.WriteLine($"{input} is a square.");
-        else
-            Console.WriteLine($"{input} is not a square.");
+
+        Console.WriteLine(IsSquare(input) ? $"{input} is a square." : $"{input} is not a square.");
     }
 }
